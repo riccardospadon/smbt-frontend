b21102c6aca31341454d6838f4c06ef707a80d54
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-07-22 20:47:40 +0200

    Implementato struttura del singolo tweet (SingleTweet)

--- src/components/SingleTweet/index.jsx
+++ src/components/SingleTweet/index.jsx
@@ -1,7 +1,27 @@
-export default function SingleTweet () {
+import { Button } from "bootstrap"
+import { Heart, HeartFill } from "react-bootstrap-icons"
+import cn from 'classnames'
+import styles from './style.module.scss'
+
+export default function SingleTweet ({ tweet, like }) {
+
+    const [liked, setLiked] = useState(false)
+
+    const handleLike = () => {
+        if(!liked) {
+            like(tweet.id)
+            setLiked(true)
+        }
+    }
+
     return(
-        <div>
-            
+        <div className={cn(styles.tweet)}>
+            <h5>{tweet.username}</h5>
+            <p>{tweet.text}</p>
+            <Button onClick={handleLike} className={cn(styles.buttonLike)}> {/* Bottone del "mi piace" */}
+                {liked ? <HeartFill color='red' /> : <Heart />}
+            </Button>
+            <span>Likes: {tweet.likes}</span>
         </div>
     )
 }
