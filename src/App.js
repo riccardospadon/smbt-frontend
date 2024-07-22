de14101f095274af54eb794eff3314503e3689db
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-07-22 15:51:20 +0200

    "dark-light" mode in base a ciò che il dispositivo usa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // Funzione che cambia il tema chiaro/scuro in base a quello impostato dal dispositivo
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark")
+    setIsDarkMode(darkModeQuery.matches)
+
+    const handleThemeChange = (event) => {
+      setIsDarkMode(event.matches)
+    }
+
+    darkModeQuery.addEventListener("change", handleThemeChange)
+    return () => {
+      darkModeQuery.removeEventListener("change", handleThemeChange)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+      <h1>Ciao</h1>
     </div>
   );
 }
